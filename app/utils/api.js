89808ff5501c2accfa51278b228147a84060fe46
Jan Elnas <jan.elnas@gmail.com>
2023-12-14 23:08:38 +0800

    refactor: fetch helper function

--- app/utils/api.js
+++ app/utils/api.js
@@ -1,118 +1,48 @@
+const fetchData = async(apiUrl)=>{
+    try{
+        const response = await fetch(apiUrl);
 
+        if(!response.ok){
+            console.log('network response not ok');
+        }
+
+        const data = await response.json();
+        return data;
+    }catch(error){
+        console.log(error);
+        return null;
+    }
+}
 export const fetchFeaturedBlog = async()=>{
     const apiUrl = "http://localhost:8000/api/blog/featured";
-
-    try{
-        const response = await fetch(apiUrl);
-
-        if(!response.ok){
-            console.log('network response not ok');
-        }
-
-        const data = await response.json();
-        return data;
-    }catch(error){
-        console.log(error);
-        return null;
-    }
+    return await fetchData(apiUrl);
 }
+
 export const fetchAllPost = async()=>{
     const apiUrl = "http://localhost:8000/api/blog"
-    try{
-        const response = await fetch(apiUrl);
-
-        if(!response.ok){
-            console.log('network response not ok');
-        }
-
-        const data = await response.json();
-        return data;
-    }catch(error){
-        console.log(error);
-        return null;
-    }
+    return await fetchData(apiUrl);
 }
+
 export const fetchAllPostOfUser = async(authorId)=>{
     const apiUrl = `http://localhost:8000/api/blog/author/${authorId}`;
-
-    try{
-        const response = await fetch(apiUrl);
-
-        if(!response.ok){
-            console.log('network response not ok');
-        }
-
-        const data = await response.json();
-        return data;
-    }catch(error){
-        console.log(error);
-        return null;
-    }
+    return await fetchData(apiUrl);
 }
 
 export const fetchPostDetail = async(postId)=>{
     const apiUrl = `http://localhost:8000/api/blog/${postId}`;
-    try{
-        const response = await fetch(apiUrl);
-
-        if(!response.ok){
-            console.log('network response not ok');
-        }
-
-        const data = await response.json();
-        return data;
-    }catch(error){
-        console.log(error);
-        return null;
-    }
+    return await fetchData(apiUrl);
 }
 
 export const fetchCategories = async()=>{
     const apiUrl = "http://localhost:8000/api/category/";
-    try{
-        const response = await fetch(apiUrl);
-
-        if(!response.ok){
-            console.log('network response not ok');
-        }
-
-        const data = await response.json();
-        return data;
-    }catch(error){
-        console.log(error);
-        return null;
-    }
+    return await fetchData(apiUrl);
 }
 
 export const fetchCategoryByName = async(categoryName)=>{
     const apiUrl = `http://localhost:8000/api/category/getCategory/${categoryName}`
-    try{
-        const response = await fetch(apiUrl);
-
-        if(!response.ok){
-            console.log('network response not ok');
-        }
-
-        const data = await response.json();
-        return data;
-    }catch(error){
-        console.log(error);
-        return null;
-    }
+    return await fetchData(apiUrl);
 }
 export const fetchPostByCategoryId = async(categoryId)=>{
     const apiUrl = `http://localhost:8000/api/blog/category/${categoryId}`
-    try{
-        const response = await fetch(apiUrl);
-
-        if(!response.ok){
-            console.log('network response not ok');
-        }
-
-        const data = await response.json();
-        return data;
-    }catch(error){
-        console.log(error);
-        return null;
-    }
+    return await fetchData(apiUrl);
 }
